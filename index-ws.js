a16b8e89029124f0df4de9c7288e81d286cb0e50
chrisroselli <croselli@contractornation.com>
2024-03-18 16:04:13 -0400

    index files

--- index-ws.js
+++ index-ws.js
@@ -8,6 +8,32 @@ app.get('/', function(req, res) {
 });
 
 server.on('request', app);
-
 server.listen(PORT, function () { console.log('Listening on ' + PORT); });
 
+/** Begin Websocket **/
+const WebsocketServer = require('ws').Server;
+const wss = new WebsocketServer({server: server});
+
+wss.on('connection', function connection(ws) {
+  const numClients = wss.clients.size;
+  console.log('Clients connected', numClients);
+  
+  wss.broadcast(`Current vistors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+   ws.send('Welcome to my server');
+  }
+
+  ws.on('close', function close() {
+    wss.broadcast(`Current vistors: ${numClients}`);
+    console.log('A client has disconnected');
+  });
+
+
+});
+
+wss.broadcast = function broadcast(data) {
+ wss.clients.forEach(function each(client) {
+  client.send(data);
+ }); 
+}
